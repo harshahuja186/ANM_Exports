71d1c86c9876985fbd61321970fd7d826ff4e211
Harsh Ahuja <harshahuja@ip-192-168-1-117.ap-south-1.compute.internal>
2025-10-09 00:14:36 +0530

    simran

---
+++ src/app/simran/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="text-3xl font-bold underline text-pink-500">
+      simran here. having fun🤩
+    </div>
+  );
+};
+
+export default page;
